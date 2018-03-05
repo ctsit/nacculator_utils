84ef0641a2729f06bc1fc7015d5f90257de33473
Roukna Sengupta <rouknasengupta@gmail.com>
2018-03-05 16:12:45 -0500

    Passing in credentials as environment variables.

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 Xvfb :99 -nolisten tcp -fbdir /var/run &
 pip install pyautogui
 cd /nacculator/get_current_data
+sed -i "s/<username>/$USERNAME/" packet_config_example.ini
+sed -i "s/<password>/$PASSWORD/" packet_config_example.ini
+sed -i "s/<gator_name>/$SMTP_USER/" smtp_config_example.ini  
+sed -i "s/<gator_password>/$SMTP_PASSWORD/" smtp_config_example.ini
+sed -i "s/<recipient>/$RECIPIENTS/" smtp_config_example.ini
 python sel.py "report"
