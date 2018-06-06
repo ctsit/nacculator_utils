ac8b25898b3d6dfd1b69a091d6a91086a9414875
Matthew McConnell <bigmatt0211@ufl.edu>
2018-06-06 08:31:10 -0400

    clean up Dockerfile and run script a bit

--- nacc_utils/nacc_automation/run.sh
+++ nacc_utils/nacc_automation/run.sh
@@ -1,5 +1,4 @@
-Xvfb :99 -nolisten tcp -fbdir /var/run &
-pip install pyautogui
+service xvfb start; export DISPLAY=:99;
 sed -i "s/<username>/$USERNAME/" packet_config_example.ini
 sed -i "s/<password>/$PASSWORD/" packet_config_example.ini
 sed -i "s/<gator_name>/$SMTP_USER/" smtp_config_example.ini  
