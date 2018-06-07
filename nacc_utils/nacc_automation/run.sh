37508c65d7a49f6df2dae230d375eabce50f0571
Matthew McConnell <bigmatt0211@ufl.edu>
2018-06-07 10:24:42 -0400

    clean up bad names and useless code while setting up env

--- nacc_utils/nacc_automation/run.sh
+++ nacc_utils/nacc_automation/run.sh
@@ -1,7 +1,7 @@
-service xvfb start; export DISPLAY=:99;
+Xvfb :99 -nolisten tcp -fbdir /var/run &
 sed -i "s/<username>/$USERNAME/" packet_config_example.ini
 sed -i "s/<password>/$PASSWORD/" packet_config_example.ini
 sed -i "s/<gator_name>/$SMTP_USER/" smtp_config_example.ini  
 sed -i "s/<gator_password>/$SMTP_PASSWORD/" smtp_config_example.ini
-sed -i "s/<recipient>/$RECIPIENTS/" smtp_config_example.ini
+sed -i "s/<recipients>/$RECIPIENTS/" smtp_config_example.ini
 python sel.py "report"
