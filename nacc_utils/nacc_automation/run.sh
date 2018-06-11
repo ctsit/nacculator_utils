3513d767539e7b54a42f53caaee6f7dc1f8a4ecc
Matthew McConnell <bigmatt0211@ufl.edu>
2018-06-11 12:23:07 -0400

    create two dockerfiles with a dynamic run script for two uses

--- nacc_utils/nacc_automation/run.sh
+++ nacc_utils/nacc_automation/run.sh
@@ -4,4 +4,5 @@ sed -i "s/<password>/$PASSWORD/" packet_config_example.ini
 sed -i "s/<gator_name>/$SMTP_USER/" smtp_config_example.ini  
 sed -i "s/<gator_password>/$SMTP_PASSWORD/" smtp_config_example.ini
 sed -i "s/<recipients>/$RECIPIENTS/" smtp_config_example.ini
-python sel.py "report"
+python sel.py "$1"
+
